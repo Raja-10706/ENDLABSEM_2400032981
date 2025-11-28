25212a01f44c17dcfe273384a7a6953e5bf45bb5
Raja-10706 <rajabattula10@gmail.com>
2025-11-28 15:04:23 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  base: '/ENDLABSEM_2400032981/',  // Replace with your actual repo name
+  plugins: [react()],
 })
